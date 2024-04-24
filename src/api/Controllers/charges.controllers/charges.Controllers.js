169786c0c12e9e298abd94e5a05a84538f196e87
guitarbuju <gerardoamadrid@gmail.com>
2024-04-24 13:03:27 +0200

    charges controllers fixed

--- src/api/Controllers/charges.controllers/charges.Controllers.js
+++ src/api/Controllers/charges.controllers/charges.Controllers.js
@@ -1,9 +1,14 @@
 import pool from "../../../DB/connection.js";
 
-export const getAllCharges = async (req, res) => {
-  try {
-    const response = await pool.query("SELECT * FROM charges");
+export const getChargesForOneInvoice = async (req, res) => {
 
+const { id }= req.params;
+
+  try {
+    const response = await pool.query(
+        "SELECT admission_id,charge_id,amount, services.price AS price,total, services.title AS service_title, p.title AS doctor FROM charges JOIN services ON charges.service_id = services.id JOIN partnershiphub AS p ON charges.collaborator = p.id WHERE admission_id = $1",
+        [id]
+      );
     response.rowCount > 0
       ? res.status(200).json({ message: "Correct Query", data: response.rows })
       : res.status(404).json({ message: "No Data Found" });
@@ -12,78 +17,36 @@ export const getAllCharges = async (req, res) => {
   }
 };
 
-export const insertCharge = async (req, res) => {
-    const { admissionId, serviceId, num, collaborator} = req.body;
-    console.log({ admissionId, serviceId, num, collaborator });
-  
-    try {
-      // Check if admission is incomplete
-      const admissionCheck = await pool.query(
-        "SELECT completed FROM admissions WHERE id = $1",
-        [admissionId]
-      );
-  
-      if (admissionCheck.rows.length === 0) {
-        // If admissionId does not exist in admissions table
-        return res.status(404).json({ message: "Admission not found" });
-      }
-  
-      if (!admissionCheck.rows[0].completed) {
-      
-        const response = await pool.query(
-          "INSERT INTO charges (admission_id, service_id, amount, collaborator) VALUES ($1, $2, $3, $4)",
-          [admissionId, serviceId, num, collaborator]
-        );
-  
-      
-        const sendBack = await pool.query(
-          "SELECT c.charge_id, admission_id, service_id, amount, total, (SELECT SUM(total) FROM charges WHERE admission_id = c.admission_id) AS total_sum FROM charges c WHERE admission_id = $1",
-          [admissionId]
-        );
-  
-        
-        const totalCharges = await pool.query(
-          "SELECT SUM(total) AS total_sum, admission_id FROM charges WHERE admission_id = $1 GROUP BY admission_id",
-          [admissionId]
-        );
-  
-       
-        return res.status(200).json({
-          message: "Charge inserted successfully",
-          data: { sendBack: sendBack.rows, total: totalCharges.rows }
-        });
-      } else {
-       
-        return res.status(400).json({ message: "Admission is already completed" });
-      }
-    } catch (error) {
-     
-      return res.status(500).json({ message: "Error inserting charge", error: error.message });
-    }
-  };
-  
-  
-  export const insertChargeLoad = async (req, res) => {
-    const dinamicInputs = req.body;
-  
-    try {
-      const script = "INSERT INTO charges (admission_id, service_id, amount, collaborator) VALUES ($1, $2, $3, $4)";
-     
-      for (const element of dinamicInputs) {
-     
-        await pool.query(script, [element.id, element.serviceId, element.num, element.value]);
-      }
-  
-      const sendBack = await pool.query("SELECT * FROM charges WHERE admission_id = $1", [dinamicInputs[0].id]);
 
-      
-      return res.status(200).json({
-        message: "Charge inserted successfully",
-        data:sendBack.rows
-      });
-    } catch (error) {
-      // Return error response
-      return res.status(500).json({ message: "Error inserting charge", error: error.message });
+export const insertChargeLoad = async (req, res) => {
+  const dinamicInputs = req.body;
+
+  try {
+    const script =
+      "INSERT INTO charges (admission_id, service_id, amount, collaborator) VALUES ($1, $2, $3, $4)";
+
+    for (const element of dinamicInputs) {
+      await pool.query(script, [
+        element.id,
+        element.serviceId,
+        element.num,
+        element.value,
+      ]);
     }
-  };
-  
+
+    const sendBack = await pool.query(
+      "SELECT admission_id,charge_id,amount, services.price AS price,total, services.title AS service_title, p.title AS doctor FROM charges JOIN services ON charges.service_id = services.id JOIN partnershiphub AS p ON charges.collaborator = p.id WHERE admission_id = $1",
+      [dinamicInputs[0].id]
+    );
+
+    return res.status(200).json({
+      message: "Charge inserted successfully",
+      data: sendBack.rows,
+    });
+  } catch (error) {
+    // Return error response
+    return res
+      .status(500)
+      .json({ message: "Error inserting charge", error: error.message });
+  }
+};
--- src/api/Routes/charges.routes/charges.routes.js
+++ src/api/Routes/charges.routes/charges.routes.js
@@ -1,12 +1,10 @@
 import express from "express";
 
-import { getAllCharges, insertCharge, insertChargeLoad } from "../../Controllers/charges.controllers/charges.Controllers.js";
+import {  getChargesForOneInvoice, insertChargeLoad } from "../../Controllers/charges.controllers/charges.Controllers.js";
 
 const chargeRouter = express.Router();
 
-chargeRouter.get('/charges', getAllCharges)
-chargeRouter.get('/charges/pending', getAllCharges)
-// chargeRouter.post('/charges', insertCharge)
+chargeRouter.get('/charges/:id', getChargesForOneInvoice)
 chargeRouter.post('/charges', insertChargeLoad)
 
 export default chargeRouter;
