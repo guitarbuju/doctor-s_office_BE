3877174cef13f4f219497a59fbdef1255e5384d2
guitarbuju <gerardoamadrid@gmail.com>
2024-07-11 18:07:04 +0200

    recipes and chart created

--- src/api/Routes/medicalChart.routes/medicalChart.routes.js
+++ src/api/Routes/medicalChart.routes/medicalChart.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { postMedicalChartData } from "../../Controllers/medicalChart.controllers/medicalChart.Controllers.js";
+import { deleteRecipeLineById, getAllRecipeLinesByAdmission, postMedicalChartData, postRecipeData } from "../../Controllers/medicalChart.controllers/medicalChart.Controllers.js";
 
 const medicalChartRouter = express.Router();
 
 medicalChartRouter.get('/medicalcharts/:id' )
 medicalChartRouter.post('/medicalcharts',postMedicalChartData )
+medicalChartRouter.post('/medicalcharts/recipe', postRecipeData )
+medicalChartRouter.get('/medicalcharts/getrecipe/:id', getAllRecipeLinesByAdmission )
+medicalChartRouter.delete('/medicalcharts/deleterecipe/:id', deleteRecipeLineById )
+
 
 export default medicalChartRouter;
