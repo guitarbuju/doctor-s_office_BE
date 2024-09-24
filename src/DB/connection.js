8fd229fff62e182f0f8bd7a708bd058995b540db
guitarbuju <gerardoamadrid@gmail.com>
2024-09-24 17:57:40 +0200

    new connection

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -15,6 +15,24 @@
 
 
 // export default pool;
+// import pkg from 'pg';
+// const { Pool } = pkg;
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const pool = new Pool({
+//   host: process.env.Hostname,
+//   user: process.env.Username,
+//   password: process.env.Password,
+//   database: process.env.Database,
+//   port: process.env.Port,
+//   ssl: { rejectUnauthorized: false }, // Ensure SSL connection for production
+// });
+
+
+// export default pool;
+
 import pkg from 'pg';
 const { Pool } = pkg;
 import dotenv from 'dotenv';
@@ -22,13 +40,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.Hostname,
-  user: process.env.Username,
-  password: process.env.Password,
-  database: process.env.Database,
-  port: process.env.Port,
-  ssl: { rejectUnauthorized: false }, // Ensure SSL connection for production
+  connectionString: process.env.Internal_Database_URL, // Use the internal URL provided by Render
+  ssl: { rejectUnauthorized: false }, // SSL for production
 });
 
+// Test the connection
+pool.connect((err) => {
+  if (err) {
+    console.error("Error connecting to PostgreSQL", err);
+  } else {
+    console.log("Connected to PostgreSQL");
+  }
+});
 
 export default pool;
