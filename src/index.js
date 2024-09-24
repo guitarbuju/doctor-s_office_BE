a8f05192b0e141e96d80bcfbe02ffc272f2e0a82
guitarbuju <gerardoamadrid@gmail.com>
2024-09-24 15:02:30 +0200

    changed ports for deployment

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ app.use(authorizationRouter);
 app.use(medicalChartRouter);
 app.use(medicinesRouter);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
