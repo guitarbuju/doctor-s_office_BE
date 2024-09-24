f4f7a9b9fa44bab0ec62d743e5ad06ca562171af
guitarbuju <gerardoamadrid@gmail.com>
2024-09-24 17:31:31 +0200

    prueba con index

--- src/api/Routes/patients.routes/patients.routes.js
+++ src/api/Routes/patients.routes/patients.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import pool from "../../../DB/connection.js"
+
 
 const patientsRouter = express.Router();
 
@@ -11,20 +11,10 @@ import {
   updatePatientById
 } from "../../Controllers/patients.controllers/patients.Controllers.js";
 
-const daPatients = async (req, res) => {
-  try {
-    console.log("Fetching all patients...");
-    const response = await pool.query("SELECT * FROM patients"); // Ensure the table name matches exactly
-    console.log("Fetched patients:", response.rows);
-    res.status(200).json({ message: "success", data: response.rows });
-  } catch (err) {
-    console.error("Error fetching patients:", err);
-    res.status(500).json({ message: "Error fetching patients" });
-  }
-};
 
 
-patientsRouter.get("/patients", daPatients);
+
+patientsRouter.get("/patients", getAllPatients);
 patientsRouter.post("/patients", createPatient);
 patientsRouter.get("/patients/:dni", getPatientByDni);
 patientsRouter.delete("/patients/delete/:dni", deletePatientByDni);
